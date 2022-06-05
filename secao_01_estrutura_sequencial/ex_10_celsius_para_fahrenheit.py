d1f779cb9ef13a9d39bc69fdbe8c0b4e48addcf4
edenbam <edenbam@gmail.com>
2022-06-05 16:08:41 -0300

    Resolvido exercício ex_10

--- secao_01_estrutura_sequencial/ex_10_celsius_para_fahrenheit.py
+++ secao_01_estrutura_sequencial/ex_10_celsius_para_fahrenheit.py
@@ -18,3 +18,6 @@ Mostrar apenas valor inteiro da temperatura
 
 def transformar_para_fahrenheit():
     """Escreva aqui em baixo a sua solução"""
+    temperatura_celsius = float(input('Digite a temperatura em fahrenheit: '))
+    temperatura_fahrenheit = temperatura_celsius * 1.8 + 32
+    print(f'Essa temperatura é de {temperatura_fahrenheit:.0f} Fahrenheit')
